d6b651e3044a9c7deb6305a2ec6ed69ecd9f9cdd
leksy-love <155977266+leksy-love@users.noreply.github.com>
2024-04-17 22:13:14 +0700

    Добавляю имя и город в файл

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from telegram import ReplyKeyboardMarkup
 from math import *
 pric = ''
-a = []
+a = ''
 gorod = ''
 reply_keyboard = [['/pogoda', '/data'],
                   ['/help']]
@@ -14,41 +14,39 @@ BOT_TOKEN = '7062541187:AAH4DuPesp2OSwIIIW3-iSZ7qYY-rrPp-Gk'
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+
+
 async def stop(update, context):
     await update.message.reply_text("Всего доброго!")
     return ConversationHandler.END
+
 
 async def help_command(update, context):
     """Отправляет сообщение когда получена команда /help"""
     await update.message.reply_text("Я расскажу тебе все о погоде сейчас. Просто введи свое имя и город")
 
 
-
 async def start(update, context):
-    global pric
+    global pric, a
     """Отправляет сообщение когда получена команда /start"""
     user = update.effective_user
+    #print(user)
     pric = str(user)
-    pric = pric.split(' ')[5]
-    pric = pric[10:]
+    pric = pric.split(',')
+    pric = pric[4]
+    pric = pric[11:]
     pric = pric[:-2]
-    a.append(pric)
-    print(pric)
+    a = pric
+    #print(pric)
     if pric == 'Hgwkx':
         await update.message.reply_html(
             rf"Привет {user.mention_html()}! Я погода-бот. Как мне тебя называть, о великий автор бота?",
         )
-    elif pric == '':
-        await update.message.reply_html(
-            rf"Привет {user.mention_html()}!",
-        )
-
     else:
         await update.message.reply_html(
             rf"Привет {user.mention_html()}! Я погода бот. Как мне тебя называть?",
         )
     return 1
-
 
 async def first_response(update, context):
     context.user_data['locality'] = update.message.text
@@ -59,6 +57,7 @@ async def first_response(update, context):
         f"В каком городе ты живешь, {name}?")
     return 2
 
+
 async def second_response(update, context):
     global gorod
     city = update.message.text
@@ -68,11 +67,11 @@ async def second_response(update, context):
     print(city)
     await update.message.reply_text(
         f"Прекрасно! сейчас посмотрим погоду в городе {city}!",
-    reply_markup=markup
+
+        reply_markup=markup
     )
     context.user_data.clear()
-    return ConversationHandler.END, city
-
+    return gorod
 
 
 async def data(update, context):
@@ -83,12 +82,18 @@ async def data(update, context):
 
 
 async def pogoda(update, context):
-    global gorod
+    global gorod, pric
     info = requests.get(
         "https://api.openweathermap.org/data/2.5/weather?q=" + gorod + "&appid=c062ce8252920e6e0f7e79b988cb9146&units=metric")
     ds = info.content.decode('UTF-8')
     ds = ds.split('\"')
     f = {}
+    with open("Names.txt", "a") as file:
+        file.write('\nГород ')
+        file.write(str(gorod))
+        file.write(', Имя ')
+        file.write(str(pric))
+        file.close()
     for i in range(len(ds)):
         if ds[i] == 'temp':
             asd = ds[i + 1]
@@ -113,8 +118,6 @@ async def pogoda(update, context):
             asd = asd[1:]
             asd = asd[:-1]
             f['Скоростьветра'] = str(asd) + 'м/с'
-        else:
-            print("Что то пошло не так")
 
     await update.message.reply_text(
         f"Погода в городе {gorod}:\n"
@@ -126,6 +129,8 @@ async def pogoda(update, context):
     )
 
     return f
+
+
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
@@ -147,6 +152,9 @@ def main():
     application.run_polling()
 
 
-
 if __name__ == '__main__':
     main()
+
+
+
+
